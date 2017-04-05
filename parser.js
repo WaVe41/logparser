a52ea48796fbcb556c06aba3fc3ee9b4889966bd
WaVe41 <farellmorrison@gmail.com>
2017-04-06 01:31:23 +0300

    final

--- parser.js
+++ parser.js
@@ -10,7 +10,6 @@ function getParsedLog() {
         head: ['IP', 'DATE', 'RESPONSE', 'USER-AGENT'],
         colWidths: [20, 30, 55, 200]
     });
-
     var lines = contents.split(/\r\n|\n\r|\r|\n/),
         length = lines.length,
         splitbyrows,
@@ -26,56 +25,57 @@ function getParsedLog() {
             [extractIpDate[0], extractIpDate[3].substring(1), splittedrow[1].substring(4), splittedrow[5]]
         );
         i++;
+
     }
 
 return table;
 }
 
 function getFirstConnections() {
-    var table = getParsedLog(),
-        length = table.length,
-        i = 0;
-
     var table1 = new Table({
         head: ['IP', 'DATE', 'RESPONSE', 'USER-AGENT'],
         colWidths: [20, 30, 55, 200]
     });
+    var table = getParsedLog(),
+        length = table.length,
+        i = 0;
 
     while (length--) {
         if (table[i][2].search(/\/books\//) !== -1) {
           table1.push([table[i][0], table[i][1],table[i][2],table[i][3]]);
-
         }
         i++;
-    }
 
-    //console.log("Total users today with bots: "+table1.length);
+    }
 
     return table1;
 }
 
 function getUsersWithoutBots() {
-    var table = getFirstConnections(),
-        length = table.length,
-        i = 0;
-
     var table1 = new Table({
         head: ['IP', 'DATE', 'RESPONSE', 'USER-AGENT'],
         colWidths: [20, 30, 55, 200]
     });
+    var table = getFirstConnections(),
+        length = table.length,
+        i = 0;
 
     while (length--) {
         if (table[i][3].search(/bot|mediapartners-google|\/slurp|\/crawler.php|ias_crawler|\/spider.php/i) === -1) {
             table1.push([table[i][0], table[i][1],table[i][2],table[i][3]]);
         }
         i++;
+
     }
-    //console.log("Total users today WITHOUT bots: "+table1.length);
 
     return table1;
 }
 
 function getUserAgents() {
+    var table1 = new Table({
+        head: ['IP', 'DATE', 'RESPONSE', 'USER-AGENT', 'BROWSER'],
+        colWidths: [20, 30, 55, 200, 60]
+    });
     var table =  getUsersWithoutBots(),
         length = table.length,
         browser_splitting,
@@ -83,33 +83,26 @@ function getUserAgents() {
         extractBrowser,
         i = 0;
 
-    var table1 = new Table({
-        head: ['IP', 'DATE', 'RESPONSE', 'USER-AGENT', 'BROWSER'],
-        colWidths: [20, 30, 55, 200, 60]
-    });
-
     while (length--) {
     browser_splitting = table[i][3].split(' ');
     split_length = browser_splitting.length - 1;
     extractBrowser = browser_splitting[split_length];
     table1.push(
         [table[i][0], table[i][1],table[i][2],table[i][3],extractBrowser]);
-
         i++;
+
     }
 
 return table1;
 }
 
 function getOS() {
-    var os_dic = ['Windows NT 6.1','Windows NT 10.0','Mac OS X','Windows NT 6.0','Windows NT 6.2',
-        'Windows NT 6.3','Windows NT 5.1','Android 4','Android 5','Android 6'];
-
     var table1 = new Table({
         head: ['IP', 'DATE', 'RESPONSE', 'USER-AGENT', 'OS'],
         colWidths: [20, 30, 55, 200, 70]
     });
-
+    var os_dic = ['Windows NT 6.1','Windows NT 10.0','Mac OS X','Windows NT 6.0','Windows NT 6.2',
+        'Windows NT 6.3','Windows NT 5.1','Android 4','Android 5','Android 6'];
     var table =  getUsersWithoutBots(),
         length = table.length,
         i = 0,
@@ -119,16 +112,16 @@ function getOS() {
     while (length--) {
         for (var j = 0; j < os_length; j++) {
             regexp = new RegExp(os_dic[j],"i");
-            //console.log(regexp);
+
         if (table[i][3].search(regexp) !== -1) {
             table1.push([table[i][0], table[i][1],table[i][2],table[i][3],os_dic[j]]);
             break;
             }
         }
     i++;
+
     }
 
-    //console.log("Num OS detected: "+table1.length);
 return table1;
 }
 
@@ -142,6 +135,7 @@ function getCountriesByIpSync() {
     while (length--) {
         query_result = sync('GET','http://freegeoip.net/json/'+table[i][0]);
         countryname = JSON.parse(query_result.getBody('utf8')).country_name;
+
         if (!clib.hasOwnProperty(countryname)) {
             clib[countryname] = 1;
             fs.writeFile('countrylib.json', JSON.stringify(clib));
@@ -151,28 +145,33 @@ function getCountriesByIpSync() {
             fs.writeFile('countrylib.json', JSON.stringify(clib));
         }
         i++;
-    }
 
+    }
 }
 
 function getCountriesByIpAsync() {
     var table = getUsersWithoutBots(),
         length = table.length-1,
         i = 0;
+
     do  {
         request('http://freegeoip.net/json/'+table[i][0],callbackForAsync);
 
         i++;
     } while (length--)
+
 }
 
 function callbackForAsync(error, response, body) {
     var countryname;
+
     if (!error && response.statusCode === 200) {
         countryname = JSON.parse(body).country_name;
+
         if (!clib.hasOwnProperty(countryname)) {
             clib[countryname] = 1;
             fs.writeFile('countrylib.json', JSON.stringify(clib));
+
         } else {
             clib[countryname] += 1;
             fs.writeFile('countrylib.json', JSON.stringify(clib));
